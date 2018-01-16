e82169032c5bf2a122bb0a1e45edbb3b9cb8db68
Evan Hsu <evanhsu@gmail.com>
2018-01-15 18:58:36 -0700

    User-configurable values have been extracted to config.sh.

--- create-swarm.sh
+++ create-swarm.sh
@@ -1,35 +1,38 @@
 #!/usr/bin/env bash
 
-DROPLET_CONFIG="\
-  --region nyc3 \
-  --image docker-16-04 \
-  --size 512mb \
-  --enable-private-networking \
-"
+## Import config variables
+source ./config.sh
 
 USAGE="Usage:
-$(basename "$0") exampleSwarmName DO_API_KEY -- Create a new swarm with a single manager node on DigitalOcean.
+$(basename "$0") exampleSwarmName [DO_ACCESS_TOKEN] -- Create a new swarm with a single manager node on DigitalOcean.
 
 where:
     exampleSwarmName    The name of the swarm. All droplets will use this name with an incrementing number appended.
-    DO_API_KEY          Your DigitalOcean API key.\n\n"
+    DO_ACCESS_TOKEN     Your DigitalOcean API key (optional). If omitted here, it must be provided in \'config.sh\'\n\n"
 
-if [ $# -eq 0 ]
-then
+## Use the appropriate DO_ACCESS_TOKEN.  The order of precedence is:
+##   1. Use the token given on the command line, if present.
+##   2. Use the token provided in `config.sh`, if present.
+##   3. Throw an error.
+
+if [[ $# -eq 0 ]]; then
     printf "$USAGE"
     exit 0
+elif [[ $# -eq 2 ]]; then
+    DO_ACCESS_TOKEN="$2"
+elif [[ -z ${DO_ACCESS_TOKEN} ]]; then
+    printf "A DigitalOcean access token was not provided.
+    You must provide one on the command line when using this command, or set one in the \'config.sh\' file.\n\n"
+    exit 1
 fi
 
-
+## Grab command-line parameters
 SWARM_NAME="$1"
-DO_API_KEY="$2"
 DROPLET_NAME="$SWARM_NAME-01"
-SSH_FINGERPRINT=$(./ssh-fingerprint.sh)
-DO_IP_DISCOVERY_URL="http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address"
 
 
 ## Check for existing droplets with this Swarm Name
-EXISTING_DROPLETS=$(doctl compute droplet list --tag-name ${SWARM_NAME} --format Name --no-header --access-token ${DO_API_KEY})
+EXISTING_DROPLETS=$(doctl compute droplet list --tag-name ${SWARM_NAME} --format Name --no-header --access-token ${DO_ACCESS_TOKEN})
 if [[ -n ${EXISTING_DROPLETS} ]]; then
     printf "\nAn existing swarm is already using the name \"${SWARM_NAME}\". The member droplets are:
 ${EXISTING_DROPLETS}\n\n" >&2
@@ -54,8 +57,13 @@ chmod a+x cloud-init/*.sh
 
 
 ## Create the new droplet
-doctl compute droplet create ${DROPLET_NAME} ${DROPLET_CONFIG} \
-    --ssh-keys ${SSH_FINGERPRINT} \
+doctl compute droplet create ${DROPLET_NAME} \
+    --image ${DO_DROPLET_IMAGE} \
+    --region ${DO_DROPLET_REGION} \
+    --size ${DO_DROPLET_SIZE} \
+    --ssh-keys ${DO_DROPLET_SSH_KEYS} \
     --tag-names "$SWARM_NAME,swarm,manager" \
-    --access-token ${DO_API_KEY} \
-    --user-data-file ./cloud-init/bootstrap.sh
+    --access-token ${DO_ACCESS_TOKEN} \
+    --user-data-file ./cloud-init/bootstrap.sh \
+    ${DO_DROPLET_FLAGS}
+
